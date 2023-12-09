59802b0e0ebc539970d632b187e737bde9efb1b7
Mel Langhoff <mlanghoff@uwalumni.com>
2023-12-08 19:26:43 -0700

    #count

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -5,4 +5,20 @@ class LinkedList
         @head = nil
     end
 
+    def append(data)
+        @head = Node.new(data)
+    end 
+
+    def count
+        if !@head = nil
+            @head = 0
+            @head += 1
+        end
+        @head
+    end
+
+    def to_string
+        
+    end
+
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -13,4 +13,26 @@ RSpec.describe LinkedList do
 
         expect(list.head).to eq nil
     end
+
+    it "#append" do
+        list = LinkedList.new
+        list.append("doop")
+
+        expect(list.head.data).to eq("doop")
+        expect(list.head.next_node).to eq nil
+    end
+
+    it "#count" do
+        list = LinkedList.new
+        list.append("doop")
+
+        expect(list.count).to eq(1)
+    end
+
+    xit "#to_string" do
+        list = LinkedList.new
+        list.append("doop")
+
+        expect(list.to_string).to eq("doop")
+    end
 end
